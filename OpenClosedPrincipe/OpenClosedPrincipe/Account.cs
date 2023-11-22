20ebe2033de28f592ed592f45bbdac2b463dde8e
Ekaterina Volkova <koweevaea@gmail.com>
2023-11-22 20:55:16 +0500

    Исходный код для рефакторинга

---
+++ OpenClosedPrincipe/OpenClosedPrincipe/Account.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Произвести рефакторинг кода в соответствии с принципом открытости/закрытости 
+/// </summary>
+namespace OpenClosedPrincipe
+{
+    public class Account
+    {
+        // тип учетной записи
+        public string Type { get; set; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+
+        // процентная ставка
+        public double Interest { get; set; }
+    }
+
+
+
+}
---
+++ OpenClosedPrincipe/OpenClosedPrincipe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Произвести рефакторинг кода в соответствии с принципом открытости/закрытости 
+/// </summary>
+namespace OpenClosedPrincipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
